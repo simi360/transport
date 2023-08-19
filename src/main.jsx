1fbdc3b250c0f9ed51d71bcaf2595ae7ab429c02
Harsimran Kaur <infamously13@gmail.com>
2023-08-18 21:08:00 -0400

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { IndexProvider } from './utils/contexts/index.context.jsx'
 import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <IndexProvider>
         <App/>
       </IndexProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
